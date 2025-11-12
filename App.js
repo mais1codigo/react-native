22b1aaee5fdce88438ab8deeed7139d3f34e4f7b
Seu Nome <seuemail@exemplo.com>
2025-11-12 14:40:42 -0300

    adicionando flatLista e ScrollView

--- App.js
+++ App.js
@@ -1,23 +1,29 @@
 import React from "react";
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, ScrollView, FlatList } from 'react-native';
 
 const App = () => {
   return (
+    <ScrollView>
     <View style={{ alignItems:"center" }}>
       <Text style={{ fontSize: 25, color: 'blue', textAlign: "center" }}>
-        Olá programadores, tudo bem?
+        Olá, tudo bem?
       </Text>
 
       <Image
         source={require('./src/barco.png')}
         style={{ width: 300, height: 300 }}
       />
+      <FlatList
+      data={[{ key: 'Arroz' }, { key: 'Feijão' }, { key: 'Açucar' }]}
+      renderItem={({ item }) => <Text>{item.key}</Text>}/>
 
+      
       <Image
         source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png' }}
         style={{ width: 400, height: 390, marginTop: 100 }}
       />
     </View>
+    </ScrollView>
   );
 };
 
